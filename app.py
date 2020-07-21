86cc8cf3eb057b144451a1a71e5ea3f6915ad743
DiptoChakrabarty <diptochuck123@gmail.com>
2020-07-21 16:36:51 +0530

    twiter data

--- app.py
+++ app.py
@@ -1,35 +1,5 @@
-import PyPDF2
-from pathlib import Path
-import os
-
-
-path=  os.environ["PWD"] + "/terms.pdf"
-print(path)
-pdffile = open(path,'rb')
-
-pdf = PyPDF2.PdfFileReader(pdffile)
-print(pdf.numPages)
-
-first = pdf.getPage(0)
-print(first.extractText())
-
-
-
-'''with open(path,'rb') as f:
-    pdf = PdfFileReader(f)
-    info = pdf.getDocumentInfo()
-    first = pdf.getPage(0)
-print(first.extractText())
-print("Author: \t", info.author)
-print()
-print("Creator: \t", info.creator)
-print()
-print("Producer: \t",info.producer)
-print()
-print("Subject: \t", info.subject)
-print()
-print("title: \t",info.title)
-print()'''
+from dotenv import load_env()
+load_env()
 
 
 
