8b9c74279b6df6bae4162426c0f7b648fedfd076
DiptoChakrabarty <diptochuck123@gmail.com>
2020-07-21 18:16:19 +0530

    import and req

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from dotenv import load_env()
 load_env()
 
+access_key = os.environ["API_KEY"]
+secret_key = os.environ["API_SECRET_KEY"]
+consumer_token = os.environ["COSUMER_TOKEN"]
+consumer_secret = os.environ["COSUMER_SECRET"]
 
 
+
+import json
+from tweepy.streaming import StreamListener
+from  tweepy import OAuthHandler
+from tweepy import Stream
+from textblob import TextBlob
+from elasticsearch import Elasticsearch
